6af4c6b1179186b16d402a7fe28ae0305f004d7b
Sean Schouten <seanmschouten@gmail.com>
2019-05-22 19:45:28 -0600

    Drills

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -2,17 +2,46 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Todo from './Components/Todo'
+
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      todoList: [],
+      userInput: '',
+    }
+  }
+
+    handleTodo(val){
+      this.setState({
+        userInput: val
+      })
+    }
+
+    addTodo = () => {
+      this.setState({
+        todoList:
+        [...this.state.todoList, this.state.userInput]
+      })
+    }
+  
+
+
+
+
+
+
+
   render() {
+    console.log(this.state.todoList)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <input type='text' placeholder='Enter new task' onChange ={(e) => this.handleTodo(e.target.value)}/>
+        <button onClick={this.addTodo}>Add</button>
+        <Todo />
       </div>
     );
   }
---
+++ app-6/src/Components/Todo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    constructor(props){
+        super(props)
+    }
+render(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
